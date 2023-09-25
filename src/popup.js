fd513783e0b7c719053046fe7f1658910d480124
hebertviana <hebert.viana@stone.com.br>
2023-09-25 00:27:49 -0300

    fix

--- src/popup.js
+++ src/popup.js
@@ -1,56 +1,42 @@
 chrome.storage.local.get({ metrics: [] }, function (result) {
     const metrics = result.metrics;
 
-    const metricsByDay = {};
-    const adSkipErrorByDay = {};
-    const elementRemoveErrorByDay = {};
-    const elementRemovedByDay = {};
+    // Crie objetos para armazenar métricas por tipo
+    const metricsByType = {
+        adSkipped: {},
+        adSkipError: {},
+        elementRemoveError: {},
+        elementRemoved: {}
+    };
 
+    // Organize as métricas por dia e tipo
     metrics.forEach(metricObj => {
         const date = new Date(metricObj.timestamp).toLocaleDateString();
-        
-        if (metricObj.metric === 'adSkipped') {
-            if (!metricsByDay[date]) {
-                metricsByDay[date] = 0;
-            }
-            metricsByDay[date]++;
-        } else if (metricObj.metric === 'adSkipError') {
-            if (!adSkipErrorByDay[date]) {
-                adSkipErrorByDay[date] = 0;
-            }
-            adSkipErrorByDay[date]++;
-        } else if (metricObj.metric === 'elementRemoveError') {
-            if (!elementRemoveErrorByDay[date]) {
-                elementRemoveErrorByDay[date] = 0;
-            }
-            elementRemoveErrorByDay[date]++;
-        } else if (metricObj.metric === 'elementRemoved') {
-            if (!elementRemovedByDay[date]) {
-                elementRemovedByDay[date] = 0;
-            }
-            elementRemovedByDay[date]++;
+
+        if (!metricsByType[metricObj.metric][date]) {
+            metricsByType[metricObj.metric][date] = 0;
         }
+        metricsByType[metricObj.metric][date]++;
     });
 
-    const uniqueDates = new Set([
-        ...Object.keys(metricsByDay),
-        ...Object.keys(adSkipErrorByDay),
-        ...Object.keys(elementRemoveErrorByDay),
-        ...Object.keys(elementRemovedByDay)
-    ]);
+    // Obtenha todas as datas únicas
+    const uniqueDates = new Set(
+        Object.keys(metricsByType.adSkipped)
+            .concat(Object.keys(metricsByType.adSkipError))
+            .concat(Object.keys(metricsByType.elementRemoveError))
+            .concat(Object.keys(metricsByType.elementRemoved))
+    );
 
+    // Ordene as datas
     const sortedDates = Array.from(uniqueDates).sort((a, b) => new Date(a) - new Date(b));
 
-    const sortedMetricsByDay = {};
-    const sortedAdSkipErrorByDay = {};
-    const sortedElementRemoveErrorByDay = {};
-    const sortedElementRemovedByDay = {};
-
-    sortedDates.forEach(date => {
-        sortedMetricsByDay[date] = metricsByDay[date] || 0;
-        sortedAdSkipErrorByDay[date] = adSkipErrorByDay[date] || 0;
-        sortedElementRemoveErrorByDay[date] = elementRemoveErrorByDay[date] || 0;
-        sortedElementRemovedByDay[date] = elementRemovedByDay[date] || 0;
+    // Crie objetos organizados por tipo e data
+    const organizedMetrics = {};
+    Object.keys(metricsByType).forEach(metricType => {
+        organizedMetrics[metricType] = {};
+        sortedDates.forEach(date => {
+            organizedMetrics[metricType][date] = metricsByType[metricType][date] || 0;
+        });
     });
 
     const ctx = document.getElementById('metricChart').getContext('2d');
@@ -60,14 +46,14 @@ chrome.storage.local.get({ metrics: [] }, function (result) {
         datasets: [
             {
                 label: 'Skip ADs',
-                data: Object.values(sortedMetricsByDay),
+                data: Object.values(organizedMetrics.adSkipped),
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1
             },
             {
                 label: 'Elements Removed',
-                data: Object.values(sortedElementRemovedByDay),
+                data: Object.values(organizedMetrics.elementRemoved),
                 backgroundColor: 'rgba(255, 206, 86, 0.2)',
                 borderColor: 'rgba(255, 206, 86, 1)',
                 borderWidth: 1
